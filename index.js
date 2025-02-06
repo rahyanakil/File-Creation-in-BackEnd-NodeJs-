e1c2ed646e544603ec18ef014ab0a1fd3796ff7e
rahyanakil <rahyanakil89@gmail.com>
2025-02-06 15:34:50 +0600

    read a file from backend

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ async function writeFileSync(){
     await fs.writeFileSync('./example.html','<h1>Hello World!</h1>')
     console.log("Successfully Written")
 }
+
+//File Read
+async function readFileSync(){
+   const data= await fs.readFileSync('example.html','utf8')
+    console.log(data)
+}
+
+
+
 (async()=>{
-    await  writeFileSync()
+    await  readFileSync()
 })()
